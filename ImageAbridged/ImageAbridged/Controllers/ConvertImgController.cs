d632ddd4f9428117fb1e00f4a089118f35a1192e
Ronald Phan <phanronald@gmail.com>
2021-04-26 22:29:41 -0700

    added webp support

--- ImageAbridged/ImageAbridged/Controllers/ConvertImgController.cs
+++ ImageAbridged/ImageAbridged/Controllers/ConvertImgController.cs
@@ -4,7 +4,9 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using ImageAbridged.Services.Core;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,10 +18,13 @@ namespace ImageAbridged.Controllers
 	public class ConvertImgController : ControllerBase
 	{
 		private readonly IHostingEnvironment _hostingEnvironment;
+		private readonly IProcessInfo _processInfo;
 
-		public ConvertImgController(IHostingEnvironment hostingEnvironment)
+		public ConvertImgController(IHostingEnvironment hostingEnvironment,
+			IProcessInfo processInfo)
 		{
 			_hostingEnvironment = hostingEnvironment;
+			_processInfo = processInfo;
 		}
 
 		[HttpPost]
@@ -41,6 +46,50 @@ namespace ImageAbridged.Controllers
 						processedImages.Add(Convert.ToBase64String(ms.ToArray()));
 					}
 				}
+			}
+
+			return Ok(processedImages);
+		}
+
+		[HttpPost]
+		[Route("ConvetImageToWebp")]
+		public IActionResult ConvertImageToWebp(IFormFile[] imageFiles)
+		{
+			var processedImages = new List<string>();
+			foreach (var imageFile in imageFiles)
+			{
+				var savedImagePath = _hostingEnvironment.ContentRootPath + "\\processedwebp\\" + imageFile.FileName;
+				var extension = Path.GetExtension(savedImagePath);
+				var savedWebpImagePath = new StringBuilder(_hostingEnvironment.ContentRootPath + "\\processedwebp\\");
+				switch (extension.ToLower())
+				{
+					case ".png":
+					case ".jpg":
+						{
+							savedWebpImagePath.Append(imageFile.FileName.Replace(extension.ToLower(), ".webp"));
+							break;
+						}
+				};
+
+				var savedOutputPath = savedWebpImagePath.ToString();
+
+				using (var stream = new FileStream(savedImagePath, FileMode.Create))
+				{
+					imageFile.CopyTo(stream);
+				}
+
+				try
+				{
+					var exePath = _hostingEnvironment.ContentRootPath + "\\imgprocess\\webp\\" + "cwebp.exe";
+					this._processInfo.ExecuteWindowRedirectProcess(exePath, string.Format("-q 80 -lossless \"{0}\" -o \"{1}\"", savedImagePath, savedOutputPath));
+				}
+				catch (Exception ex)
+				{
+					System.Diagnostics.Debug.WriteLine(ex.ToString());
+				}
+
+				var processedPngFile = System.IO.File.ReadAllBytes(savedOutputPath);
+				processedImages.Add(Convert.ToBase64String(processedPngFile));
 			}
 
 			return Ok(processedImages);
--- ImageAbridged/ImageAbridged/Operations/FileUploadOperation.cs
+++ ImageAbridged/ImageAbridged/Operations/FileUploadOperation.cs
@@ -11,12 +11,27 @@ namespace ImageAbridged.Operations
 	{
 		public void Apply(Operation operation, OperationFilterContext context)
 		{
-			if(operation.OperationId.ToLower() == "apicompressimgcompresspngpost")
+			if(operation.OperationId.ToLower() == "compresspng")
 			{
 				operation.Parameters.Clear();
 				operation.Parameters.Add(new NonBodyParameter
 				{
 					Name = "pngImgFile",
+					In = "formData",
+					Description = "Upload File",
+					Required = true,
+					Type = "file"
+				});
+
+				operation.Consumes.Add("multipart/form-data");
+			}
+
+			if(operation.OperationId.ToLower() == "convertimagetowebp")
+			{
+				operation.Parameters.Clear();
+				operation.Parameters.Add(new NonBodyParameter
+				{
+					Name = "imageFiles",
 					In = "formData",
 					Description = "Upload File",
 					Required = true,
